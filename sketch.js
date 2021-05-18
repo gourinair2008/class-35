7eb335457512575aee63d6f4f708af556f355dc8
GOURI NAIR <75208319+gourinair2008@users.noreply.github.com>
2021-05-18 19:31:47 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,12 @@
+var gameState=0
+var playerCount
+var database
+var form,player,game
+
+function setup() {
+    createCanvas(400,400)
+    database=firebase.database()
+}
+function draw() {
+    
+}
